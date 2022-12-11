5df2daf8b2d15ca5b459d621fd112b343ae02e5a
Cristian Holzmann <109118554+cristhol@users.noreply.github.com>
2022-12-11 03:46:02 -0300

    segunda entrega

--- newcomputer-vite/src/Components/ItemCount/ItemCount.jsx
+++ newcomputer-vite/src/Components/ItemCount/ItemCount.jsx
@@ -1,36 +1,34 @@
-import React, { useState } from 'react'
+import { useState } from 'react'
 
-const ItemCount = (stock=10, initial=1, onAdd) => {
-    const [ count, setCount ] = useState(0)
+
+
+const ItemCount = ({ stock = 10, initial = 1, onAdd }) => {
+    const [count, setCount] = useState(initial)
 
     const restar = () => {
-        if (count > initial){
-            setCount(count-1) 
+        if (count > initial) {
+            setCount(count - 1)
         }
     }
     const sumar = () => {
-        if (count > stock){
-        setCount(count+1)
+        if (count < stock) {
+            setCount(count + 1)
+        }
     }
-}
+    return (
+        <div className="card m-5">
+            <div className="card-header">
+                <label htmlFor="">{count}</label>
+            </div>
+            <div className="card-body">
+                <button onClick={sumar} className="btn btn-outline-primary">+</button>
+                <button onClick={restar} className="btn btn-outline-primary">-</button>
+            </div>
+            <div className="card-footer">
+                <button className="btn btn-outline-success" onClick={() => onAdd(count)}> Agregar al carrito</button>
+            </div>
 
-const handleOnAdd = () => {
-    onAdd (count)
-}
-return (
-    <div className="card m-5">
-        <div className="card-header">
-            <label htmlFor="">{count}</label>
         </div>
-        <div className="card-body">
-            <button onClick={sumar} className="btn btn-outline-primary">+</button>
-            <button onClick={restar} className="btn btn-outline-primary">-</button>
-        </div>
-        <div className="card-footer">
-            <button className="btn btn-outline-success" onClick={  handleOnAdd }> Agregar al carrito</button>
-        </div>
-
-    </div>
     )
 }
 
